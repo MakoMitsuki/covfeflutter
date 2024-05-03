2d80e8a416a699d93c9187747ae01f6de24b5ce5
Mika <makomitsuki@gmail.com>
2024-05-03 13:02:25 +0100

    working navigation

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:covfeflutter/components/bottom_nav_bar.dart';
 import 'package:covfeflutter/const.dart';
+import 'package:covfeflutter/pages/cartpage.dart';
+import 'package:covfeflutter/pages/shoppage.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,11 +12,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // navigateBottomBar
+  int _selectedIndex = 0;
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages
+  final List<Widget> _pages = [
+    ShopPage(),
+    CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: backgroundColor,
-      bottomNavigationBar: BottomNavBar(),
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
